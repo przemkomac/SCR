4f93d63210ea5e5d4858c400a4e1401b2ee4b55e
Przemys³aw Maciejewski <przemkomac@o2.com>
2021-01-26 21:18:05 +0100

    new viewmodels

---
+++ SCR/Models/DeadlineSchedulingParametersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace WebApp.Models
+{
+    public class PrioritySchedulingParametersViewModel
+    {
+        [DisplayName("Czas przetwarzania")]
+        public int Capacity { get; set; }
+
+        [DisplayName("Okres")]
+        public int Period { get; set; } 
+
+        [DisplayName("Termin")]
+        public int Deadline { get; set; }
+    }
+}
---
+++ SCR/Models/PrioritySchedulingParametersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace WebApp.Models
+{
+    public class DeadlineSchedulingParametersViewModel
+    {
+        [DisplayName("Czas przetwarzania")]
+        public int Capacity { get; set; }
+
+        [DisplayName("Okres")]
+        public int Period { get; set; }
+
+        [DisplayName("Priorytet")]
+        public int Priority { get; set; }
+    }
+}
